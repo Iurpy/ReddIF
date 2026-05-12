b833d99610ee72b39f7c3fdc1451507788798c22
lanai-giani <lanaigiani11@gmail.com>
2026-05-12 19:27:42 -0300

    corrige Community.cs

--- ReddIF/Models/Community.cs
+++ ReddIF/Models/Community.cs
@@ -1,28 +1,23 @@
-using System.Collections.ObjectModel;
 using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
 
-
 namespace ReddIF.Models;
 
-public class Community: BaseModel
+[Table("community")]
+public class Community : BaseModel
 {
-    public Community()
-    {
-        Users = new Collection<User>();
-    }
-    
-    [PrimaryKey("",false)]
-    public int CommunityId {get; set;}
-    
+    [PrimaryKey("communityid", false)]
+    public int CommunityId { get; set; }
+
     [Column("dono")]
     public int OwnerId { get; set; }
 
+    [Column("nome")]
+    public string Nome { get; set; } = string.Empty;
+
+    [Column("description")]
+    public string Description { get; set; } = string.Empty;
+
+    [Column("createtime")]
     public DateTime CreateTime { get; set; } = DateTime.Now;
-    
-    public required string Nome {get; set;} = string.Empty;
-    
-    public string Description { get; set; } = string.Empty; 
-    public ICollection<User> Users { get; set; }
-    
 }
